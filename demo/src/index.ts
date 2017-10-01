3ce26da606574df1aed9ecb33d1379b9b8d76be2
Bruce Hem <bruce.hem@pathmazing.com>
2017-10-01 11:37:31 +0700

    add support for matching controllers with glob pattern

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -2,10 +2,9 @@ import 'reflect-metadata';
 
 import * as Koa from 'koa';
 import * as logger from 'koa-logger';
+import { resolve as pathResolve } from 'path';
 
 import { useKoaMods } from '../../dist';
-import { AuthController } from './api/auth/auth.controller';
-import { TodosController } from './api/todos/todos.controller';
 import { sequelize } from './common';
 import { authorizationChecker } from './common/access-control';
 import { validationCustomMessages } from './common/custom-messages';
@@ -19,7 +18,7 @@ app.use(errorHandler());
 
 useKoaMods({
   app,
-  controllers: [ AuthController, TodosController ],
+  controllers: [ pathResolve(__dirname, 'api/**/*.controller.ts') ],
   authorizationChecker,
   customValidateOptions: {
     rules: validationCustomRules,
@@ -29,7 +28,7 @@ useKoaMods({
 
 (async () => {
   await sequelize.sync({ force: false });
-  app.listen(6969, () => console.log('Server running on port 6969'));
+  app.listen(6969, () => console.log('🎉🎉🎉 Server running on port 6969 🎉🎉🎉'));
 })().catch(e => {
   console.error(e);
   process.exit(1);
--- src/lib/bindings.ts
+++ src/lib/bindings.ts
@@ -1,4 +1,5 @@
 import * as debug from 'debug';
+import { Glob } from 'glob';
 import * as Koa from 'koa';
 import * as bodyparser from 'koa-bodyparser';
 import * as Router from 'koa-router';
@@ -8,11 +9,26 @@ import { AUTH_META_KEY, ROUTES_META_KEY } from './constants';
 
 export function useKoaMods(options: KoaModsOptions): any {
   const log = debug('koa-mods');
-  const { app, controllers, authorizationChecker, customValidateOptions } = options;
+  const { app, authorizationChecker, customValidateOptions } = options;
+  let { controllers } = options;
 
   app.use(bodyparser());
 
   const router = new Router();
+
+  if (controllers.length === 1) {
+    const files = new Glob(controllers[0], { sync: true });
+    log('found the following controller files: \n', files.found);
+    controllers = files.found.map(f => {
+      const ctrlObj = require(f);
+      for (const prop in ctrlObj) {
+        if (ctrlObj.hasOwnProperty(prop)) {
+          return ctrlObj[prop];
+        }
+      }
+    });
+  }
+  log('controllers', controllers);
 
   for (const ctrl of controllers) {
     const customValMw = async (ctx: any, next: () => Promise<any>) => {
@@ -20,35 +36,49 @@ export function useKoaMods(options: KoaModsOptions): any {
       return next();
     };
 
-    const routes = Reflect.getMetadata(ROUTES_META_KEY, ctrl);
-    for (const route of routes) {
-      const { method, url, name, params } = route;
-      let { middleware } = route;
+    try {
+      const routes = Reflect.getMetadata(ROUTES_META_KEY, ctrl);
+      if (routes) {
+        for (const route of routes) {
+          const { method, url, name, params } = route;
+          let { middleware } = route;
 
-      middleware = [ customValMw, ...middleware ];
+          middleware = [ customValMw, ...middleware ];
 
-      const authFnMws: { type: 'required' | 'optional'; roles: string[] } | undefined = Reflect.getMetadata(
-        AUTH_META_KEY,
-        ctrl.prototype,
-        name
-      );
+          const authFnMws: { type: 'required' | 'optional'; roles: string[] } | undefined = Reflect.getMetadata(
+            AUTH_META_KEY,
+            ctrl.prototype,
+            name
+          );
 
-      if (authorizationChecker && authFnMws) {
-        const am = async (ctx: Koa.Context, next: () => Promise<any>) => {
-          ctx.state.authUser = await authorizationChecker(ctx, authFnMws);
-          return next();
-        };
-        middleware = [ am, ...middleware ];
+          if (authorizationChecker && authFnMws) {
+            const am = async (ctx: Koa.Context, next: () => Promise<any>) => {
+              ctx.state.authUser = await authorizationChecker(ctx, authFnMws);
+              return next();
+            };
+            middleware = [ am, ...middleware ];
+          }
+
+          log(
+            `${authFnMws && authFnMws.type === 'required'
+              ? '🛡️ '
+              : authFnMws && authFnMws.type === 'optional' ? '🛡️* ' : ' '}routes:`,
+            (method as string).toUpperCase(),
+            url,
+            '-->',
+            name
+          );
+          router[method](url, ...middleware, async function(ctx: Koa.Context, next: () => Promise<any>) {
+            const inst = new ctrl();
+            const args = getArguments(params, ctx, next);
+            const result = inst[name](...args);
+            if (result) ctx.body = await result;
+            return result;
+          });
+        }
       }
-
-      log('routes:', (method as string).toUpperCase(), url, '-->', name);
-      router[method](url, ...middleware, async function(ctx: Koa.Context, next: () => Promise<any>) {
-        const inst = new ctrl();
-        const args = getArguments(params, ctx, next);
-        const result = inst[name](...args);
-        if (result) ctx.body = await result;
-        return result;
-      });
+    } catch (error) {
+      throw new Error('Invalid controllers provided.');
     }
   }
 
